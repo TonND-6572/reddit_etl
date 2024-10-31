82d4a51efb8d95f27445b5f06675cb62db79903d
TonND-6572 <toiiuvn111@gmail.com>
2024-10-31 08:39:33 +0700

    modify dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,16 +2,17 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import altair as alt
-
 import sys
 import os
+
+from datetime import datetime
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from etls.aws_glue_service import get_from_data_catalog, connect_to_botos3, extract_data
 
 st.set_page_config(
-    page_title="US Population Dashboard",
+    page_title="Reddit Data engineer session Dashboard",
     page_icon="🏂",
     layout="wide",
     initial_sidebar_state="expanded")
@@ -45,7 +46,7 @@ df = transform_n_validate(df)
 
 
 with st.sidebar:
-    st.title('🏂 US Population Dashboard')
+    st.title('🏂 Reddit Data engineer session Dashboard')
     
     month_list = list(df['month'].unique())[::-1]
     month_list.append('All month')
@@ -64,31 +65,43 @@ with col[0]:
 
     # number of post each day
     st.header('Number of post each day')
-    group_by_date = df.groupby(['year', 'month', 'day']) \
+    cutoff = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+    upto_last_date = df[df['created_utc'] < cutoff]
+    group_by_date = upto_last_date.groupby(['year', 'month', 'day']) \
         .agg(number_of_author=('author', 'count'), number_of_post=('title', 'count'))
     group_by_date.reset_index(inplace=True)
     line_fig = px.line(group_by_date, 'day', 'number_of_post')
     st.plotly_chart(line_fig)
 
 with col[1]:
+    # number of user
+    user_count = df['author'].nunique()
+    st.metric(label='number of user', value=user_count)
+
     # top 5 post 
-    st.header('Top 5 post')
+    st.markdown('### Top 5 post score')
     sort_by_score = df.sort_values(by=['score'], ascending=False)
-    st.dataframe(sort_by_score.iloc[:5])
+    st.dataframe(sort_by_score[['title', 'score'    , 'url']].iloc[:5])
 
     # top 5 user 
-    st.header('Top 5 user')
+    st.markdown('### Top 5 user')
     
     group_by_user = df.groupby('author') \
-        .agg({'title': 'count', 'score': 'mean'}) \
-        .sort_values(['title', 'score'], ascending=[False, False])
+        .agg(
+            number_of_post = ('title', pd.Series.nunique), 
+            score_mean = ('score', 'mean')
+        ) \
+        .sort_values(['number_of_post', 'score_mean'], ascending=[False, False])
 
-    st.dataframe(group_by_user.iloc[:5])
+    st.dataframe(group_by_user[:5])
 
     # recent 5 post
-    st.header('Recent 5 post')
+    st.markdown('### Recent 5 post')
     sort_by_time = df.sort_values('created_utc', ascending=False)
-    st.dataframe(sort_by_time.iloc[:5])
+    
+    st.dataframe(sort_by_time[['created_utc', 'title', 'author', 'url']].iloc[:5])
+
 
 st.markdown('## Explore data')
-st.dataframe(df)
+st.dataframe(df.sort_values('created_utc', ascending=False).drop(['year', 'month', 'day'], axis=1))
